4ac305343ec470ddcf342db30bc12bd6da9018ab
dominic <dominic0312@126.com>
2014-08-01 16:32:08 +0800

    iteration 3

--- app/controllers/month_deposits_controller.rb
+++ app/controllers/month_deposits_controller.rb
@@ -1,7 +1,47 @@
 class MonthDepositsController < ResourcesController
+  def settle
+    load_collection
+  end
 
-  #private
-   # def invest_params
-    #  params.require(:month_deposit).permit(:deposit_number, :transaction_number, :address, :usage, :usage_detail, :credit_level, :annual_rate, :amount, :repayment_period, :repayment_method, :each_repayment_amount, :free_invest_amount, :invest_end_date, :remark, :status)
-	#	end
+  def switchdisplay
+    dep = MonthDeposit.find(params[:format])
+    if dep.display == "show"
+      dep.display = "hide"
+    else
+      dep.display = "show"
+    end
+    dep.save!
+    redirect_to settle_month_deposits_path
+  end
+
+  def publish
+    dep = MonthDeposit.find(params[:format])
+    load_object
+    dep.stage = "融资中"
+    dep.display = "show"
+    dep.save!
+    redirect_to settle_month_deposits_path
+  end
+
+  def finish
+    dep = MonthDeposit.find(params[:format])
+    dep.stage = "收益中"
+    dep.save!
+    redirect_to settle_month_deposits_path
+  end
+
+  def refund
+    dep = MonthDeposit.find(params[:format])
+    Invest.where(:loan_number => dep.deposit_number).each { |inv| inv.refund }
+    dep.stage = "已结束"
+    dep.save!
+    redirect_to settle_month_deposits_path
+  end
+
+
+  private
+  def invest_params
+    params.require(:month_deposit).permit(:deposit_number, :transaction_number, :address, :usage, :usage_detail, :credit_level, :annual_rate, :amount, :repayment_period, :repayment_method, :each_repayment_amount, :free_invest_amount, :invest_end_date, :remark, :status)
+  end
+
 end
