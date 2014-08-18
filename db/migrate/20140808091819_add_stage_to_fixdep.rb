081e0deab8c982aadbcc890c5b8dd4f42ea6cc9d
dominic <dominic0312@126.com>
2014-08-18 16:49:00 +0800

    no comment

--- db/migrate/20140808091819_add_stage_to_fixdep.rb
+++
@@ -1,5 +0,0 @@
-class AddStageToFixdep < ActiveRecord::Migration
-  def change
-    add_column :fixed_deposits, :stage, :string, :default => "未发布"
-  end
-end
--- db/migrate/20140814040017_add_fixed_amount_to_fixdep.rb
+++
@@ -1,5 +0,0 @@
-class AddFixedAmountToFixdep < ActiveRecord::Migration
-  def change
-    add_column :fixed_deposits, :fixed_amount, :decimal, :default => 0
-  end
-end
