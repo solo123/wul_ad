10d8bad4f28ec9636073277cc6995b97a3d060d5
dominic <dominic0312@126.com>
2014-08-28 17:39:57 +0800

    no comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = Employee.new
+u.email = "dominic0312@126.com"
+u.password = u.password_confirmation ="wqxyy1985"
+u.save!
+
+0.upto(20) do |i|
+  FixedDeposit.create(deposit_number: "FX20140821" + i.to_s, total_amount: 120000, annual_rate:13, repayment_period: 3,
+                      guarantee:"ICBC", free_invest_amount: 120000, detail:"no detail", income_method:"tax", join_date:"2014-08-11",
+                      join_condition:"car", expiring_date:"2014-09-09",repayment_method:"bycash", premature_redemption:"yes",
+                      fee:12)
+
+end
