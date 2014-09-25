675e748e2b5122db36b5019ff4a58cf35728fb6d
dominic <dominic0312@126.com>
2014-09-25 14:36:09 +0800

    no comment

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
-# Airbrake.configure do |config|
-#   config.api_key = '283c842ed955b77fa71590f6b32d43df'
-#   config.host    = '127.0.0.1'
-#   config.port    = 3002
-#   config.secure  = config.port == 443
-#   config.development_environments = []
-# end
+Airbrake.configure do |config|
+  config.api_key = '283c842ed955b77fa71590f6b32d43df'
+  config.host    = '127.0.0.1'
+  config.port    = 3002
+  config.secure  = config.port == 443
+  config.development_environments = []
+end
