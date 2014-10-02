241b93bcb5eadacddc7b4d701d74aa8d7cd0c535
root <root@AY131212154326764dfcZ.(none)>
2014-10-02 13:30:47 +0800

    no comment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,8 +21,8 @@ pid DEPLOY_PATH+"/shared/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path DEPLOY_PATH+"/log/wooul.stderr.log"
-stdout_path DEPLOY_PATH+"/log/wooul.stdout.log"
+stderr_path DEPLOY_PATH+"/shared/log/wooul.stderr.log"
+stdout_path DEPLOY_PATH+"/shared/log/wooul.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
