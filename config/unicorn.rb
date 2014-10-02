8df9c4d35f35b22d3123ba483d3ea03152d53f5d
root <root@AY131212154326764dfcZ.(none)>
2014-10-02 11:27:21 +0800

    no comment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,45 +1,28 @@
-# Sample verbose configuration file for Unicorn (not Rack)
-#
-# This configuration file documents many features of Unicorn
-# that may not be needed for some applications. See
-# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
-# for a much simpler configuration file.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-
-# Use at least one worker per core if you're on a dedicated server,
-# more will usually help for _short_ waits on databases/caches.
 worker_processes 4
 
 # Since Unicorn is never exposed to outside clients, it does not need to
-# run on the standard HTTP port (80), there is no reason to start Unicorn
-# as root unless it's from system init scripts.
-# If running the master process as root and the workers as an unprivileged
-# user, do this to switch euid/egid in the workers (also chowns logs):
-# user "unprivileged_user", "unprivileged_group"
 
-# Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-APP_PATH = "/home/wooul/wooul.com"
+APP_PATH = "/home/wooul/wooul.com/current"
+DEPLOY_PATH = "/home/wooul/wooul.com"
 working_directory APP_PATH# available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen APP_PATH+"/shared/tmp/sockets/unicorn.sock", :backlog => 64
+listen DEPLOY_PATH+"/shared/tmp/sockets/unicorn.sock", :backlog => 64
 
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid APP_PATH+"/shared/tmp/pids/unicorn.pid"
+pid DEPLOY_PATH+"/shared/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path APP_PATH+"/shared/log/wooul.stderr.log"
-stdout_path APP_PATH+"/shared/log/wooul.stdout.log"
+stderr_path DEPLOY_PATH+"/shared/log/wooul.stderr.log"
+stdout_path DEPLOY_PATH+"/shared/log/wooul.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
